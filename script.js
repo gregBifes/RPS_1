5602cb345a6801e4c3d8e9686cc301507e523f17
gregBifes <gregbifes@gmail.com>
2025-06-24 17:59:58 +0200

    Game logic added
    playGame function created with score and console.log() descriptions

--- script.js
+++ script.js
@@ -1,19 +1,72 @@
 let rockBtn = document.getElementById('rock-btn');
 let paperBtn = document.getElementById('paper-btn');
 let scissorsBtn = document.getElementById('scissors-btn');
-
+let userWeapon = '';
+let computerWeapon = '';
 let score = {
     'wins': 0,
     'draws': 0,
     'loses': 0,
 }
 let weapons = {
-    'rock': 0,
-    'paper': 1,
-    'scissors': 2,
+    0: 'rock',
+    1: 'paper',
+    2: 'scissors',
+
 }
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
-let randomInteger = getRandomInt(3);
+
+function playGame(userWeapon) {
+
+    let randomInteger = getRandomInt(3);
+    computerWeapon = weapons[randomInteger];
+
+    if (userWeapon === 'rock') {
+        if (computerWeapon === 'rock') {
+            console.log(`It's a Tie ${userWeapon}   ${computerWeapon}`);
+            score.draws += 1;
+        }
+        else if (computerWeapon === 'paper') {
+            console.log(`You Lose! ${userWeapon}   ${computerWeapon}`);
+            score.loses += 1;
+        }
+        else {
+            console.log(`You Win! ${userWeapon}   ${computerWeapon}`);
+            score.wins += 1;
+        }
+    }
+    if (userWeapon === 'paper') {
+        if (computerWeapon === 'paper') {
+            console.log(`It's a Tie ${userWeapon}   ${computerWeapon}`);
+            score.draws += 1;
+        }
+        else if (computerWeapon === 'scissors') {
+            console.log(`You Lose! ${userWeapon}   ${computerWeapon}`);
+            score.loses += 1;
+        }
+        else {
+            console.log(`You Win! ${userWeapon}   ${computerWeapon}`);
+            score.wins += 1;
+        }
+    }
+    if (userWeapon === 'scissors') {
+        if (computerWeapon === 'scissors') {
+            console.log(`It's a Tie ${userWeapon}   ${computerWeapon}`);
+            score.draws += 1;
+        }
+        else if (computerWeapon === 'rock') {
+            console.log(`You Lose! ${userWeapon}   ${computerWeapon}`);
+            score.loses += 1;
+        }
+        else {
+            console.log(`You Win! ${userWeapon}   ${computerWeapon}`);
+            score.wins += 1;
+        }
+    }
+}
+
+
 
